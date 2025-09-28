308331b16069fc98c75e938b8c44725f61ba95a4
hojun <ghwns0787@gmail.com>
2025-09-28 21:30:42 +0900

    JWT 인증 및 로그인 중

--- src/main/java/com/mockerview/config/SecurityConfig.java
+++ src/main/java/com/mockerview/config/SecurityConfig.java
@@ -2,23 +2,56 @@ package com.mockerview.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.mockerview.jwt.JWTFilter;
+import com.mockerview.jwt.JWTUtil;
+import com.mockerview.jwt.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception{
+                
+                return configuration.getAuthenticationManager();
+    }
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(JWTUtil jwtUtil
+                        , AuthenticationConfiguration authenticationConfiguration){
+        this.jwtUtil                     = jwtUtil;
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
             .csrf(csrf -> csrf.disable())
-            .headers(headers -> headers.disable());
-        
+            .headers(headers -> headers.disable())
+            .formLogin(login -> login.disable())
+            .httpBasic(auth -> auth.disable())
+       
+            .addFilterAt(new LoginFilter(
+                authenticationManager(authenticationConfiguration), jwtUtil
+            ), UsernamePasswordAuthenticationFilter.class)
+
+            .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class)
+            .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+            
         return http.build();
     }
 
---
+++ src/main/java/com/mockerview/dto/RegisterDTO.java
@@ -0,0 +1,15 @@
+package com.mockerview.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDTO {
+
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String role;
+}
