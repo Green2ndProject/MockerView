50bea093747e7d3f6634e42110d5c2133f5fadc8
서원준 <hnk1203@naver.com>
2025-10-10 16:34:48 +0900

    feat: 리뷰목록에 제목뜨게 하기

--- src/main/java/com/mockerview/repository/ReviewRepository.java
+++ src/main/java/com/mockerview/repository/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.mockerview.repository;
 
 import com.mockerview.entity.Review;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
@@ -8,6 +10,7 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findBySessionId(Long sessionId);
+    @EntityGraph(attributePaths = {"session", "reviewer"})
     List<Review> findByReviewerId(Long reviewerId);
     List<Review> findByAnswerId(Long answerId);
 }
