2fa86105929ae4a1d8ccc20aeff2ee8fbc1aff55
dakyumlee <oicrcutie@gmail.com>
2025-10-13 15:37:14 +0900

    fix:배포오류수정

--- src/main/java/com/mockerview/entity/User.java
+++ src/main/java/com/mockerview/entity/User.java
@@ -3,7 +3,6 @@ package com.mockerview.entity;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.*;
-
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -47,19 +46,11 @@ public class User {
     @Column(name = "IS_DELETED", nullable = false)
     @ColumnDefault("0")
     @Builder.Default
-    private Short isDeleted = 0;
+    private Integer isDeleted = 0;
 
     @Column(name = "DELETED_AT")
     private LocalDateTime deletedAt;
 
     @Column(name = "WITHDRAWAL_REASON", length = 255)
     private String withdrawalReason;
-    
-    public boolean isDeleted() {
-        return isDeleted != null && isDeleted == 1;
-    }
-    
-    public void setDeleted(boolean deleted) {
-        this.isDeleted = deleted ? (short) 1 : (short) 0;
-    }
 }
