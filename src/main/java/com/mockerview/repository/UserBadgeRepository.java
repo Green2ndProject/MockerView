c22c0f749d945e337c9e19f9d9b90c4854df0f37
“dakyumlee” <oicrcutie@gmail.com>
2025-11-11 12:29:18 +0900

    fix:mypage

--- src/main/java/com/mockerview/repository/UserBadgeRepository.java
+++ src/main/java/com/mockerview/repository/UserBadgeRepository.java
@@ -7,12 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UserBadgeRepository extends JpaRepository<UserBadge, Long> {
+    List<UserBadge> findByUserId(Long userId);
     List<UserBadge> findByUserOrderByEarnedAtDesc(User user);
-    Optional<UserBadge> findByUserAndBadgeType(User user, BadgeType badgeType);
     boolean existsByUserAndBadgeType(User user, BadgeType badgeType);
     long countByUser(User user);
 }
